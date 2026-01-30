71730df5f2cee5e49c4058718764efb21048a5a2
LIKHIN K N <likhinkn6@gmail.com>
2026-01-30 17:24:57 +0530

    sum of N number

---
+++ program3.js
@@ -0,0 +1,13 @@
+//sum of  n numbers
+// let num = 5;
+// let sum= 0;
+// for (let i = 1; i <= num ; i++){
+//     sum +=i
+// }
+// console.log(sum);
+
+
+// another method 
+let n = 10 ;
+let sum1 = (n*(n+1))/ 2;
+console.log(sum1);
