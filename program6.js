5377b58246502ce975104cd5317d7dcc089d9445
LIKHIN K N <likhinkn6@gmail.com>
2026-01-31 12:38:04 +0530

    print square of N numbers

---
+++ program6.js
@@ -0,0 +1,12 @@
+// print square of N  numbers
+
+let n = 5;
+ for(let i= 0; i <=n ; i++){
+    //method 1
+  //  console.log(i ** 2);
+   // method 2
+    console.log(Math.pow(i,2));
+    
+    
+    
+ }
