c71678f8667810720c6b04aa18c56548e7aa8b81
LIKHIN K N <likhinkn6@gmail.com>
2026-02-03 16:11:47 +0530

    check if a character is uppercase, lowercase, digit, or special character

---
+++ program16.js
@@ -0,0 +1,16 @@
+function Ischar(char){
+    if(char >= "A" && char <= "Z"){
+        return "UpperCase"
+    }else if( char >= "a" && char <="z"){
+        return "Lowercase"
+    }else if( char >= "0" && char  <= "9"){
+        return "digit"
+    }
+    return "special character"
+
+    
+}
+console.log(Ischar("a"));
+console.log(Ischar("B"));
+console.log(Ischar("4"));
+console.log(Ischar("@"));
