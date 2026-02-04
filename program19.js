eba53d838f396f20df381734bd1255bd86a05de7
LIKHIN K N <likhinkn6@gmail.com>
2026-02-04 11:30:38 +0530

    print inverted right angled triangle

---
+++ program19.js
@@ -0,0 +1,10 @@
+// print inverted right angled triangle
+let  n =5;
+for (let i = n; i>=1 ; i--){
+    let str = "";
+    for( let j = 1 ; j<= i ;j++){
+        str = str + "*"
+    }
+    console.log(str);
+    
+}
