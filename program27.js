e880d98bcf4f4c7fc6f2ea025fd10872deacd3ec
LIKHIN K N <likhinkn6@gmail.com>
2026-02-05 17:11:01 +0530

    Diamond pattern

---
+++ program27.js
@@ -0,0 +1,30 @@
+let n=3;
+for(let i=1;i<=n;i++){
+    let str=""
+    for(let j=1;j<=n-i;j++){
+        str+=" "
+    }
+    for(let s=1;s<=2*i-1;s++){
+        
+            str+="*";
+        }
+      
+        console.log(str);
+    }
+    
+
+for(let i=n-1;i>=1;i--){
+    let str=""
+    for(let j=1;j<=n-i;j++){
+        str+=" "
+    }
+    for(let s=1;s<=2*i-1;s++){
+        
+            str+="*";
+        }
+        
+       
+    
+    console.log(str);
+    
+}
