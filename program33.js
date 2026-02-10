cc11b63e70c1531d458606e3f154efacb23a2ab3
LIKHIN K N <likhinkn6@gmail.com>
2026-02-10 12:30:24 +0530

    Factorial Without Loop (Using Recursion)

---
+++ program33.js
@@ -0,0 +1,9 @@
+//Factorial Without Loop (Using Recursion)
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
