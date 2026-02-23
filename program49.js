444459c05114ac376a234b981f9d425fb10e93ac
LIKHIN K N <likhinkn6@gmail.com>
2026-02-23 21:44:13 +0530

    best time to buy and sell stock

---
+++ program49.js
@@ -0,0 +1,19 @@
+function maxProfit(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];   // buy at lowest price
+        } 
+        else {
+            let profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+    }
+
+    return maxProfit;
+}
+
+console.log(maxProfit([7,1,5,3,6,4]));
